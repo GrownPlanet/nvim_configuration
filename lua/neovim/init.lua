95f9efcccb7525bf47a49653d0bef1be54b30db8
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-05-04 20:46:45 +0200

    upload all files

---
+++ lua/neovim/init.lua
@@ -0,0 +1,3 @@
+require("neovim.packer")
+require("neovim.set")
+
---
+++ lua/neovim/set.lua
@@ -0,0 +1,13 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true 
+
+vim.opt.guicursor = ""
+
+vim.opt.wrap = false
+
+vim.opt.scrolloff = 8
+
+vim.api.nvim_exec('language en_US', true)
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
