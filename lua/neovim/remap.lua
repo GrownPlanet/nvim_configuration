09171ac21417b1b6f7df30c93a5335dbddc343e3
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-05-04 21:05:58 +0200

    cool new commands

--- lua/neovim/remap.lua
+++ lua/neovim/remap.lua
@@ -1,2 +1,7 @@
+vim.keymap.set("n", "K", ":tabnext<enter>")
 vim.keymap.set("n", "J", ":-tabnext<enter>")
-vim.keymap.set("n", "K", ":+tabnext<enter>")
+
+vim.keymap.set("n", "L", ":tabnew<enter>")
+vim.keymap.set("n", "H", ":-tabnew<enter>")
+
+vim.keymap.set("n", "X", ":tabclose<enter>")
