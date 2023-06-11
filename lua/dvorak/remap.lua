48f1854dd3c51d7b1614983429a913d1094f7166
Timo dehandschutter <timo.dehandschutter@gmail.com>
2023-06-11 12:47:23 +0200

    ello

--- lua/dvorak/remap.lua
+++ lua/dvorak/remap.lua
@@ -1,28 +1,11 @@
-vim.keymap.set("n", "N", vim.cmd.tabnext)
-vim.keymap.set("n", "D", vim.cmd.tabprevious)
+vim.keymap.set("n", "L", vim.cmd.tabnext)
+vim.keymap.set("n", "H", vim.cmd.tabprevious)
 
 vim.keymap.set("n", "X", vim.cmd.tabclose)
 
 vim.keymap.set("n", "K", "")
 
-vim.keymap.set("n", ",,", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", ",f", vim.cmd.NvimTreeFocus)
+vim.keymap.set("n", "tt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "tf", vim.cmd.NvimTreeFocus)
 
-vim.keymap.set("n", "yg", vim.cmd.UndotreeToggle)
-
-vim.keymap.set('n', 'd', 'h')
-vim.keymap.set('n', 'h', 'j')
-vim.keymap.set('n', 't', 'k')
-vim.keymap.set('n', 'n', 'l')
-
-vim.keymap.set('n', 'k', 'd')
-
-vim.keymap.set('v', 'd', 'h')
-vim.keymap.set('v', 'h', 'j')
-vim.keymap.set('v', 't', 'k')
-vim.keymap.set('v', 'n', 'l')
-
-vim.keymap.set('v', 'k', 'd')
-
-vim.keymap.set('n', 'q', 'n')
-vim.keymap.set('n', 'Q', 'N')
+vim.keymap.set("n", "yf", vim.cmd.UndotreeToggle)
