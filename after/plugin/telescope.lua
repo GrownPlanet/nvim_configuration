47f0c8b7ab4cf1777c9c71bc03ea7c027ae1fc61
Timo dehandschutter <timo.dehandschutter@gmail.com>
2023-06-11 12:43:42 +0200

    afad

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'll', builtin.find_files, {})
+vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
